62c80031ec6fb204b1098b3ae9fe501327688e5b
Alvaro Rubina <alvarorubina132@gmail.com>
2025-03-16 22:04:10 -0300

    Creado login.js

---
+++ js/login.js
@@ -0,0 +1,35 @@
+document.querySelector("#loginBtn").addEventListener("click", autenticarUsuario);
+
+async function autenticarUsuario(event) {
+    event.preventDefault();
+    let username = document.querySelector("#username").value;
+    let password = document.querySelector("#password").value;
+
+    if (!username || !password) {
+        alert("Complete todos los campos antes de continuar");
+        return;
+    }
+
+    try {
+        const response = await fetch(`http://181.111.166.250:8081/tp/login.php?user=${username}&pass=${password}`);
+        const data = await response.json();
+
+        const respuesta = data.respuesta;
+        const mensaje = data.mje;
+
+        switch (respuesta) {
+            case "OK":
+                alert(mensaje)
+                break;
+            case "ERROR":
+                alert(mensaje)
+                break;
+        }
+    } catch (error) {
+        console.log("Error en la autenticacion: " + error)
+        alert("Hubo un problema al autenticar")
+    }
+
+    
+
+}
