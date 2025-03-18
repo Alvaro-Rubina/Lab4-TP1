a3355026674c3decef3c75f8fc8c635d3b4bed01
Alvaro Rubina <alvarorubina132@gmail.com>
2025-03-18 01:18:09 -0300

    Agregado metodo para cambiar el estado de bloqueado

--- js/lista.js
+++ js/lista.js
@@ -33,7 +33,7 @@ async function buscarPorUsuario(event) {
         llenarTabla(data);
         
     } catch (error) {
-        console.error('Hubo un error al realizar la consulta:', error);
+        console.error('Hubo un error al traer los registros:', error);
     }
 }
 
@@ -41,17 +41,77 @@ async function llenarTabla(data) {
     const tbody = document.querySelector("table.table tbody");
     tbody.innerHTML = "";
 
-    data.forEach(element => {
+    data.forEach(elemento => {
         const row = document.createElement("tr");
+
+        // si bloqueado = Y la fila es roja, si bloqueado = N la fila es verde
+        if (elemento.bloqueado === 'Y') {
+            row.classList.add('red-row');
+        } else if (elemento.bloqueado === 'N') {
+            row.classList.add('green-row');
+        }
+
         row.innerHTML = `
-        <td>${element.id}</td>
-        <td>${element.usuario}</td>
-        <td>${element.bloqueado}</td>
-        <td>${element.apellido}</td>
-        <td>${element.nombre}</td>
+        <td>${elemento.id}</td>
+        <td>${elemento.usuario}</td>
+        <td>${elemento.bloqueado}</td>
+        <td>${elemento.apellido}</td>
+        <td>${elemento.nombre}</td>
         <td><button class="btn btn-block">Bloquear</button></td>
         <td><button class="btn btn-unblock">Desbloquear</button></td>
         `;
         tbody.appendChild(row);
     });
+
+    // agrego eventos a los botones que se agregaron a la tabla
+    document.querySelectorAll(".btn-block").forEach(button => {
+        button.addEventListener("click", bloquearUsuario);
+    });
+    document.querySelectorAll(".btn-unblock").forEach(button => {
+        button.addEventListener("click", desbloquearUsuario);
+    });
+}
+
+async function bloquearUsuario(event) {
+    event.preventDefault();
+
+    const boton = event.target;
+    const fila = boton.closest("tr");
+    const id = fila.querySelector("td").textContent;
+
+    try {
+        const respuesta = await fetch(`http://181.111.166.250:8081/tp/lista.php?action=BLOQUEAR&idUser=${id}&estado=Y`)
+        if (respuesta.ok) {
+            // llamo de vuelta para reflejar los cambios
+            await traerRegistros();
+            
+        } else {
+            console.error('Hubo un error al bloquear el usuario');
+        }
+
+    } catch (error) {
+        console.error('Hubo un error al realizar la consulta:', error);
+    }
+    
+}
+
+async function desbloquearUsuario(event) {
+    event.preventDefault();
+
+    const boton = event.target;
+    const fila = boton.closest("tr");
+    const id = fila.querySelector("td").textContent;
+
+    try {
+        const respuesta = await fetch(`http://181.111.166.250:8081/tp/lista.php?action=BLOQUEAR&idUser=${id}&estado=N`);
+        if (respuesta.ok) {
+            // llamo de vuelta para reflejar los cambios
+            await traerRegistros();
+
+        } else {
+            console.error('Hubo un error al desbloquear el usuario');
+        }
+    } catch (error) {
+        console.error('Hubo un error al realizar la consulta:', error);
+    }
 }
